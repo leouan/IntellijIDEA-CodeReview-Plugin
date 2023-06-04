acf892e4228db93a7b7be38d6d6a100c42871f94
Youxian Le <youxian.le@shopee.com>
2023-06-04 18:39:57 +0800

    init fix

--- src/com/veezean/idea/plugin/codereviewer/action/LeftMarkIconProvider.java
+++ src/com/veezean/idea/plugin/codereviewer/action/LeftMarkIconProvider.java
@@ -24,7 +24,7 @@ import java.util.Collection;
  */
 public class LeftMarkIconProvider extends RelatedItemLineMarkerProvider {
     @Override
-    protected void collectNavigationMarkers(@NotNull PsiElement element, @NotNull Collection<? super RelatedItemLineMarkerInfo> result) {
+    protected void collectNavigationMarkers(@NotNull PsiElement element, @NotNull Collection<? super RelatedItemLineMarkerInfo<?>> result) {
 
         if (!(element instanceof PsiWhiteSpace)) {
             super.collectNavigationMarkers(element, result);
